d6b35f73801ebeb50a9f84a0975fb156397d24d4
AquaSecure <100585091+AquaSecure@users.noreply.github.com>
2023-07-05 21:53:40 +0000

    personal

---
+++ page-generator/htmlGeneratorTest.py
@@ -0,0 +1,124 @@
+
+#
+# Copyright [2012] [Ali Ok - aliok@apache.org]
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+
+# coding=utf-8
+import sys
+import unittest
+import htmlGenerator
+
+class HtmlGeneratorTest(unittest.TestCase):
+
+    def setUp(self):
+        super(HtmlGeneratorTest, self).setUp()
+
+    def testSplitLine(self):
+        splits = htmlGenerator.splitLine(u'Teferrüc eyleyi vardım')
+        assert splits ==[u'Teferrüc', u' ', u'eyleyi', u' ' , u'vardım'], splits
+
+        splits = htmlGenerator.splitLine(u'Teferrüc, eyleyi vardım')
+        assert splits ==[u'Teferrüc', u',', u'', u' ', u'eyleyi', u' ' , u'vardım'], splits
+
+        splits = htmlGenerator.splitLine(u'Teferrüc, ,eyleyi vardım')
+        assert splits ==[u'Teferrüc', u',', u'', u' ', u'', u',', u'eyleyi', u' ' , u'vardım'], splits
+
+        splits = htmlGenerator.splitLine(u'Teferrüc, ,eyleyi. vardım?')
+        assert splits ==[u'Teferrüc', u',', u'', u' ', u'', u',', u'eyleyi', u'.', u'', u' ', u'vardım', u'?', u''], splits
+
+        splits = htmlGenerator.splitLine(u'Teferrüc, ,eyleyi-vardım?')
+        assert splits ==[u'Teferrüc', u',', u'', u' ', u'', u',', u'eyleyi', u'-', u'vardım', u'?', u''], splits
+
+    def testGetHtmlForWord_directMatch(self):
+        word = u'Teferrüc'
+        html = htmlGenerator.getHtmlForWord(word)
+        assert html==u'<a href="#" class="tip" title="teferrüc : gezinti">Teferrüc</a>', html
+
+    def testGetHtmlForWord_rootMatch(self):
+        word = u'sinleri'
+        html = htmlGenerator.getHtmlForWord(word)
+        assert html==u'<a href="#" class="tip" title="sin : mezar">sin</a>leri', html
+
+    def testGetHtmlForWord_rootMatch2(self):
+        word = u'mihnette'
+        html = htmlGenerator.getHtmlForWord(word)
+        assert html==u'<a href="#" class="tip" title="mihnet : zahmet, eziyet, gam, keder, sıkıntı, bela">mihnet</a>te', html
+
+    def testGetHtmlForWord_verbRootMatch(self):
+        word = u'kılıp'
+        html = htmlGenerator.getHtmlForWord(word)
+        assert html==u'<a href="#" class="tip" title="kılmak : etmek, eylemek, yapmak">kıl</a>ıp', html
+
+    def testGetHtmlForWord_verbRootMatch2(self):
+        word = u'kılanlara'
+        html = htmlGenerator.getHtmlForWord(word)
+        assert html==u'<a href="#" class="tip" title="kılmak : etmek, eylemek, yapmak">kıl</a>anlara', html
+
+    def testGetHtmlForWord_empty(self):
+        assert htmlGenerator.getHtmlForWord(u'') == u''
+        assert htmlGenerator.getHtmlForWord(u' ') == u' '
+        assert htmlGenerator.getHtmlForWord(u'  \t  ') == u'  \t  '
+        assert htmlGenerator.getHtmlForWord(u'  \t , ') == u'  \t , '
+        assert htmlGenerator.getHtmlForWord(u',  \t  ') == u',  \t  '
+        assert htmlGenerator.getHtmlForWord(u', ,') == u', ,'
+        assert htmlGenerator.getHtmlForWord(u', \t ,') == u', \t ,'
+
+    def testGetHtmlForLine_withoutPunctuation(self):
+        line = u'Teferrüc sinlerine kılanlar iyi mi acaba?'      #sentence doesn't make any sense!
+        html = htmlGenerator.getHtmlForLine(line)
+        assert html==u'<p class="line"><a class="lineTipToggler" href="#"><img src="../resources/img/lineTip.png"/></a>' \
+                     u'<a href="#" class="tip" title="teferrüc : gezinti">Teferrüc</a> <a href="#" class="tip" title="sin : mezar">sin</a>lerine ' \
+                     u'<a href="#" class="tip" title="kılmak : etmek, eylemek, yapmak">kıl</a>anlar iyi mi acaba?</p>', repr(html)
+
+    def testGetHtmlForLine_withPunctuation1(self):
+        line = u'Teferrüc sinlerine , kılanlar iyi mi acaba?'      #sentence doesn't make any sense!
+        html = htmlGenerator.getHtmlForLine(line)
+        assert html==u'<p class="line"><a class="lineTipToggler" href="#"><img src="../resources/img/lineTip.png"/></a>'\
+                     u'<a href="#" class="tip" title="teferrüc : gezinti">Teferrüc</a> <a href="#" class="tip" title="sin : mezar">sin</a>lerine , '\
+                     u'<a href="#" class="tip" title="kılmak : etmek, eylemek, yapmak">kıl</a>anlar iyi mi acaba?</p>', repr(html)
+
+    def testGetHtmlForLine_withPunctuation2(self):
+        line = u'Teferrüc sinlerine, kılanlar iyi mi acaba?'      #sentence doesn't make any sense!
+        html = htmlGenerator.getHtmlForLine(line)
+        assert html==u'<p class="line"><a class="lineTipToggler" href="#"><img src="../resources/img/lineTip.png"/></a>'\
+                     u'<a href="#" class="tip" title="teferrüc : gezinti">Teferrüc</a> <a href="#" class="tip" title="sin : mezar">sin</a>lerine, '\
+                     u'<a href="#" class="tip" title="kılmak : etmek, eylemek, yapmak">kıl</a>anlar iyi mi acaba?</p>', repr(html)
+
+    def testGetHtmlForLine_withPunctuation3(self):
+        line = u'Teferrüc ,sinlerine, kılanlar iyi mi acaba?'      #sentence doesn't make any sense!
+        html = htmlGenerator.getHtmlForLine(line)
+        assert html==u'<p class="line"><a class="lineTipToggler" href="#"><img src="../resources/img/lineTip.png"/></a>'\
+                     u'<a href="#" class="tip" title="teferrüc : gezinti">Teferrüc</a> ,<a href="#" class="tip" title="sin : mezar">sin</a>lerine, '\
+                     u'<a href="#" class="tip" title="kılmak : etmek, eylemek, yapmak">kıl</a>anlar iyi mi acaba?</p>', repr(html)
+
+    def testGetHtmlForLine_withPunctuation3(self):
+        line = u'Teferrüc,sinlerine, kılanlar iyi mi acaba?'      #sentence doesn't make any sense!
+        html = htmlGenerator.getHtmlForLine(line)
+        assert html==u'<p class="line"><a class="lineTipToggler" href="#"><img src="../resources/img/lineTip.png"/></a>'\
+                     u'<a href="#" class="tip" title="teferrüc : gezinti">Teferrüc</a>,<a href="#" class="tip" title="sin : mezar">sin</a>lerine, '\
+                     u'<a href="#" class="tip" title="kılmak : etmek, eylemek, yapmak">kıl</a>anlar iyi mi acaba?</p>', repr(html)
+
+    def testGetHtmlCouplet(self):
+        line1 = u'sinlerine,'      #sentence doesn't make any sense!
+        line2 = u'sinlerini.'      #sentence doesn't make any sense!
+        html = htmlGenerator.getHtmlForCouplet([line1, line2])
+        assert html==u'<p class="line"><a class="lineTipToggler" href="#"><img src="../resources/img/lineTip.png"/></a>'\
+                     u'<a href="#" class="tip" title="sin : mezar">sin</a>lerine,</p>'\
+                     u'\n'\
+                     u'<p class="line"><a class="lineTipToggler" href="#"><img src="../resources/img/lineTip.png"/></a>'\
+                     u'<a href="#" class="tip" title="sin : mezar">sin</a>lerini.</p>' , repr(html)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ page-generator/pageGenerator.py
@@ -0,0 +1,143 @@
+
+#
+# Copyright [2012] [Ali Ok - aliok@apache.org]
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+
+# coding=utf-8
+import os
+import shutil
+import sys
+import coupletCreator
+
+import htmlGenerator
+
+def getDataFileNameList(dataFolder, dataFileExtension):
+    dataFiles = os.listdir(dataFolder)
+    return filter(lambda fileName: fileName.endswith(dataFileExtension), dataFiles)
+
+
+def getPageHref(dataFileName):
+    return dataFileName + '.html'
+
+
+def getTitle(lines, fileIndex):
+    firstLine = lines[0]
+    if not firstLine[-1].isalpha():
+        firstLine = firstLine[:-1]
+
+    return str(fileIndex) + '. ' + firstLine
+
+
+def clearDestinationFolder(destinationFolderName):
+    if os.path.exists(destinationFolderName):
+        shutil.rmtree(destinationFolderName)
+        os.makedirs(destinationFolderName)
+
+
+def readPageTemplate():
+    pageTemplateFile = open('templates/pageTemplate.html')
+    try:
+        pageTemplateFile = open('templates/pageTemplate.html')
+        template = pageTemplateFile.read()
+        return template
+    finally:
+        pageTemplateFile.close()
+
+
+def generatePageContent(template=None, lines=None, title=None, outputFileName=None, nextPageHref=None):
+    couplets = coupletCreator.createCouplets(lines)
+
+    htmlForFile = htmlGenerator.getHtmlForCouplets(couplets)
+
+    html = template
+    html = html.replace('#{title}', title.encode('utf-8'))
+    html = html.replace('#{content}', htmlForFile.encode('utf-8'))
+    html = html.replace('#{titleVar}', title.encode('utf-8'))
+    html = html.replace('#{urlVar}', outputFileName)
+    html = html.replace('#{nextPageUrl}', nextPageHref)
+
+    return html
+
+
+def writeToFile(outputFilePath, content):
+    outputFile = open(outputFilePath, 'w')
+    try:
+        outputFile.write(content)
+    finally:
+        outputFile.close()
+
+
+def readLines(dataFilePath):
+    dataFile = open(dataFilePath)
+    try:
+        return [line.decode('utf-8').strip() for line in dataFile.readlines()]
+    finally:
+        dataFile.close()
+
+
+def createIndexFile(indexContent, destinationFolderName):
+    indexTemplateFile = open('templates/indexTemplate.html')
+    indexOutputFile = open(destinationFolderName + 'index.html', 'w')
+
+    try:
+        indexTemplate = indexTemplateFile.read()
+        html = indexTemplate.replace('#{index}', indexContent.encode('utf-8'))
+        indexOutputFile.write(html)
+    finally:
+        indexOutputFile.close()
+        indexTemplateFile.close()
+
+
+def main():
+    dataFolder = '../data/'
+    dataFileExtension = '.txt'
+    destinationFolderName = '../generated-pages/pages/'
+
+    clearDestinationFolder(destinationFolderName)
+
+    pageTemplate = readPageTemplate()
+
+    dataFileNameList = getDataFileNameList(dataFolder, dataFileExtension)
+
+    indexContent = ''
+
+    for i in range(0, len(dataFileNameList)):
+        dataFileName = dataFileNameList[i]
+        print 'Processing input file ' + dataFileName
+
+        fileIndex = i + 1
+
+        nextPageHref = ''
+        if i < len(dataFileNameList) - 1: #not the last element
+            nextPageHref = getPageHref(dataFileNameList[i + 1])
+
+        outputFileName = getPageHref(dataFileName)
+
+        # we'll use lines more than once, so let's get them on memory
+        lines = readLines(dataFolder + dataFileName)
+
+        title = getTitle(lines, fileIndex)
+
+        html = generatePageContent(pageTemplate, lines, title, outputFileName, nextPageHref)
+
+        writeToFile(destinationFolderName + outputFileName, html)
+
+        indexContent += '<li><a href="' + outputFileName + '">' + title + '</a></li>\n'
+
+
+    createIndexFile(indexContent, destinationFolderName)
+
+if __name__ == "__main__":
+    sys.exit(main())
