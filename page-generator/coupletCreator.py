b96e4a84c174876bed9dbdb9f4ff2f1c0fdcc339
AquaSecure <100585091+AquaSecure@users.noreply.github.com>
2023-07-05 18:34:55 +0000

    Update character

--- page-generator/coupletCreator.py
+++ page-generator/coupletCreator.py
@@ -54,4 +54,41 @@ def _verifyCoupletSize(lines, coupletSize):
         raise repr('Couplet size ' + str(coupletSize) + ' cannot be verified for title : ' + lines[0])
     for i in range(coupletSize, len(lines), (coupletSize + 1)):
         if lines[i] != '':
-          
+            raise repr('Couplet size ' + str(coupletSize) + ' cannot be verified for title : ' + lines[0])
+
+
+def _removeEmptyLines(lines):
+    return filter(lambda (s): s != '', lines)
+
+def _createCoupletsList(lines, coupletSize):
+    couplets = []
+    for i in range(0, len(lines) / coupletSize):
+        couplet = []
+        for j in range(0, coupletSize):
+            couplet.append(lines[coupletSize * i + j])
+        couplets.append(couplet)
+
+    return couplets
+
+def _mergeLines(firstLine, secondLine):
+    if not firstLine[-1].isalpha():
+        firstLine = firstLine[:-1]
+
+    indexOfFirstSpaceInSecondLine = secondLine.find(' ')
+    if indexOfFirstSpaceInSecondLine != -1:
+        firstWordOfSecondLine = secondLine[0:indexOfFirstSpaceInSecondLine]
+        if firstWordOfSecondLine[0].isupper() and not nlpToolAdapter.isProperNoun(firstWordOfSecondLine):
+            secondLine = secondLine[0].lower() + secondLine[1:]
+
+    return firstLine + ', ' + secondLine
+
+
+def _changeCoupletSizeToTwo(couplets):
+    #Some hardcoded stuff, I feel sorry :(
+
+    newCouplets = []
+    for couplet in couplets:
+        newCouplet = [_mergeLines(couplet[0], couplet[1]), _mergeLines(couplet[2], couplet[3])]
+        newCouplets.append(newCouplet)
+
+    return newCouplets
